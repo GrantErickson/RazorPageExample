6be71abf99e10a23ed8c2af503badd9dd13da521
Grant Erickson <grant@granterickson.com>
2022-03-02 22:07:47 -0800

    Add project files.

---
+++ RazorPageExample/Api/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RazorPageExample.Api
+{
+    [Route("api/values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("name")]
+        public string GetName()
+        {
+            return "Inigo";
+        }
+
+        [HttpGet("List")]
+        public IEnumerable<string> List()
+        {
+            return new string[] { "Grant", "Jonikka", "Mark" };
+        }
+
+        [HttpGet("Weather")]
+        public Weather Weather(int? number)
+        {
+            return new Weather
+            {
+                Name = "Test",
+                Description = "Cloudy",
+                Number = number
+            };
+        }
+
+
+
+    }
+    public class Weather
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int? Number { get; set; }
+    }
+}
